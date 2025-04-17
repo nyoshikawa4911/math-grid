b4023a7767b56e6d38e5c6f639560a572088b5c9
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-17 12:16:18 +0900

    インタラクティブなモード選択を実装

---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+import enquirer from "enquirer";
+
+const MENU_ID_GRID_5X5 = 1;
+const MENU_ID_GRID_10X10 = 2;
+const MENU_ID_QUIT = 3;
+
+const DIGIT_ID_1 = 1;
+const DIGIT_ID_2 = 2;
+
+const menuQuestions = [
+  {
+    type: "select",
+    name: "id",
+    message: "Please select a menu.",
+    choices: [
+      { id: MENU_ID_GRID_5X5, name: "5 x 5 Grid", description: "Start the game with 25 cells." },
+      {
+        id: MENU_ID_GRID_10X10,
+        name: "10 x 10 Grid",
+        description: "Start the game with 100 cells.",
+      },
+      { id: MENU_ID_QUIT, name: "Quit", description: "Exit the game." },
+    ],
+    footer() {
+      return this.selected.description;
+    },
+    result() {
+      return this.selected.id;
+    },
+  },
+];
+
+const digitQuestions = [
+  {
+    type: "select",
+    name: "id",
+    message: "Choose the number of digits for calculations.",
+    choices: [
+      { id: DIGIT_ID_1, name: "1 Digits", description: "Calculate with single-digit numbers." },
+      { id: DIGIT_ID_2, name: "2 Digits", description: "Calculate with two-digit numbers." },
+    ],
+    footer() {
+      return this.selected.description;
+    },
+    result() {
+      return this.selected.id;
+    },
+  },
+];
+
+console.clear();
+console.log("---------- Welcome to Math Grid ----------\n");
+
+const menuAnswer = await enquirer.prompt(menuQuestions);
+if (menuAnswer.id === MENU_ID_QUIT) {
+  process.exit(0);
+}
+
+const digitAnswer = await enquirer.prompt(digitQuestions);
