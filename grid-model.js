79deed87231ee88cae731bb869b0181f1828f437
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-19 23:54:33 +0900

    データ管理クラスとしてGridModelクラスを作成

---
+++ grid-model.js
@@ -0,0 +1,22 @@
+export default class GridModel {
+  #gridWidth;
+  #maxDigits;
+  #grid;
+  #targetRows;
+  #targetCols;
+
+  constructor(gridWidth, maxDigits) {
+    this.#gridWidth = gridWidth;
+    this.#maxDigits = maxDigits;
+  }
+
+  setup() {
+    this.#grid = new Array(this.#gridWidth).fill().map(() => new Array(this.#gridWidth).fill(null));
+    this.#targetRows = new Array(this.#gridWidth).fill().map(() => this.#randomInt());
+    this.#targetCols = new Array(this.#gridWidth).fill().map(() => this.#randomInt());
+  }
+
+  #randomInt() {
+    return Math.floor(Math.random() * (this.#maxDigits === 1 ? 10 : 100));
+  }
+}
