09e386bd949c74384a4764144fdaed7757fccd01
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-20 01:00:49 +0900

    GridModelの初期化処理の呼び出しを追加

--- math-grid.js
+++ math-grid.js
@@ -1,17 +1,21 @@
+import GridModel from "./grid-model.js";
 import KeyInput from "./key-input.js";
 
 export default class MathGrid {
   #gridWidth;
   #maxDigits;
+  #gridModel;
   #keyInput;
 
   constructor(gridWidth, maxDigits) {
     this.#gridWidth = gridWidth;
     this.#maxDigits = maxDigits;
+    this.#gridModel = new GridModel(gridWidth, maxDigits);
     this.#keyInput = new KeyInput();
   }
 
   async start() {
+    this.#gridModel.setup();
     await this.#keyInput.waitKeyInput();
   }
 }
