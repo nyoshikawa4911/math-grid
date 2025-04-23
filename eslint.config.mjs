3244913021085ab2ed44d436d898b3c5a2646d51
nyoshikawa4911 <nyoshikawa4911@gmail.com>
2025-04-23 13:24:55 +0900

    vitest, eslint-plugin-vitest を導入

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,22 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import vitest from "@vitest/eslint-plugin";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node } },
   eslintConfigPrettier,
+  {
+    files: ["__tests__/**"],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...vitest.environments.env.globals,
+      },
+    },
+  },
 ]);
